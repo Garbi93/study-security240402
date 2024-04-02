5cb15918d55d6878a7f841e94e558d4d48170cb9
Garbi93 <iseung2093@gmail.com>
2024-04-02 19:16:30 +0900

    04/02 세션 소멸 시간 설정, 다중로그인, 세션 고정 보호

--- src/main/java/com/example/studysecurity240402/config/SecurityConfig.java
+++ src/main/java/com/example/studysecurity240402/config/SecurityConfig.java
@@ -42,6 +42,16 @@ public class SecurityConfig {
         // 로그인을 하게 되면 csrf 라는 토큰이 필요한데 지금 과정에서는 disable 상태로 두고 개발함
         http.csrf((auth) -> auth.disable());
 
+        // 다중 로그인 설정
+        http.sessionManagement((auth) -> auth
+                .maximumSessions(1) // <- 하나의 아이디에 동시접속 수
+                .maxSessionsPreventsLogin(true) // <- true: 해당 값을 초과할 경우 새 로그인 차단
+                // false: 기존 로그인 아웃 새로그인 진행
+        );
+
+        // 세션 고정 보호
+        http.sessionManagement((auth) -> auth.sessionFixation().changeSessionId());
+
         return http.build(); // http 데이터를 받은 것을 build 로 리턴 해야한다.
     }
 
