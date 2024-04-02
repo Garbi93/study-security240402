2d1bc12864d90fb26275f51f71e7a632d79dce3f
Garbi93 <iseung2093@gmail.com>
2024-04-02 15:34:32 +0900

    04/02 시큐리티에 BCrpyt 암호화 셋팅

--- src/main/java/com/example/studysecurity240402/config/SecurityConfig.java
+++ src/main/java/com/example/studysecurity240402/config/SecurityConfig.java
@@ -4,11 +4,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration // <- 스프링 부트 에서 config 파일 인식 시키는 기능
 @EnableWebSecurity // <- 시큐리티 기능을 활성화 하고 웹 보안 구성을 시작할 수 있다.
 public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -36,4 +43,6 @@ public class SecurityConfig {
 
         return http.build(); // http 데이터를 받은 것을 build 로 리턴 해야한다.
     }
+
+
 }
