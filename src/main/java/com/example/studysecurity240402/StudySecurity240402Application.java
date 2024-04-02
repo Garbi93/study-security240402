f7a45ab53eb2192c5581b361676fe973b0711f8a
Garbi93 <iseung2093@gmail.com>
2024-04-02 13:34:20 +0900

    04/02 security project setting

---
+++ src/main/java/com/example/studysecurity240402/StudySecurity240402Application.java
@@ -0,0 +1,13 @@
+package com.example.studysecurity240402;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySecurity240402Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudySecurity240402Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/studysecurity240402/StudySecurity240402ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studysecurity240402;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySecurity240402ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
